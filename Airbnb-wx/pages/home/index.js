21e7269864222eeded4cbe1ec789015d2e67a676
beichenloveNacy <857560476@qq.com>
2019-12-03 18:53:19 +0800

    day-two

--- Airbnb-wx/pages/home/index.js
+++ Airbnb-wx/pages/home/index.js
@@ -1,40 +1,71 @@
 // pages/home/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    advData: []
+    advData:[],
+    interval: 5000,
+    current: 0,
+    latitude: '',
+    longitude: ''
   },
   requestAdvertise(){
     wx.request({
       url: 'http://localhost:3000/indexPage',
       success: (res) => {
-        this.advData = res.data
+        const advData = res.data.advertise
+        this.setData({
+          advData
+        })
       }
     })
   },
+  currentHandle(e){
+    let {current} = e.detail
+    this.setData({
+      current
+    })
+  },
+  //获取用户地理位置
+  getLocation(){
+    console.log(1)
+    wx.getLocation({
+      type: 'wgs84',
+      altitude: true,
+      success: (res) => {
+        const latitude = res.latitude
+        const longitude = res.longitude
+        console.log(res)
+        this.setData({
+          latitude,
+          longitude
+        })
+      },
+      fail: () => {},
+      complete: () => {}
+    });
+  },
+ 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     this.requestAdvertise()
-    // console.log(this.advData)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    let vm = this
+    vm.getLocation()
   },
 
   /**
