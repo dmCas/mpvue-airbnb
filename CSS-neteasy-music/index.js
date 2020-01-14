a56e45a3272d4493c9e8d1e0f6753348cb896740
BeichenloveNancy <857560476@qq.com>
2020-01-14 19:22:04 +0800

    1

---
+++ CSS-neteasy-music/index.js
@@ -0,0 +1,7 @@
+      
+  var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'vertical', // 垂直切换选项
+    loop: true, // 循环模式选项
+    
+  })        
+  
--- leetcode/HDU 算法题/test.js
+++
@@ -1,5 +0,0 @@
-var a = [1, 2 ,3]
-for(let i=0; i<a.length;i++){
-  a.splice(a.indexOf(Math.max(...a)),1)
-  console.log(a)
-}
