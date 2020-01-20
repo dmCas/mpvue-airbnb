8025732bf6a1be1807a16937a74d591161aa59ae
BeichenloveNancy <857560476@qq.com>
2020-01-20 14:24:01 +0800

    mpvue-map

--- mpvue/my-project/src/components/location.vue
+++ mpvue/my-project/src/components/location.vue
@@ -16,37 +16,88 @@
       </div>
     </div>
     <div class="map">
-      <map id="map" longitude="120.6020740000" latitude="31.3119640000" 
-      :markers="markers"  
-      scale="14"  
-      style="width: 100%; height: 200px;"
-      :enable-zoom="enablezoom"
-      :enable-scroll="enablescroll"
-      :enable-rotate="enablerotate"></map>
+      <map
+        @click="getSelect"
+        id="map"
+        longitude="120.6020740000"
+        latitude="31.3119640000"
+        :markers="markers"
+        scale="14"
+        style="width: 100%; height: 200px;"
+        :enable-zoom="enablezoom"
+        :enable-scroll="enablescroll"
+        :enable-rotate="enablerotate"
+      ></map>
     </div>
     <div class="map-more">
-      <img src="../../static/images/site.png" alt="">
-      <span>预定后将显示详细地址</span></div>
+      <img src="../../static/images/site.png" alt />
+      <span>预定后将显示详细地址</span>
+    </div>
   </div>
 </template>
 
 <script>
+import QQMapWX from "../utils/map";
+    var qqmapsdk;
+    qqmapsdk = new QQMapWX({
+    key:'23SBZ-AJ36F-3GXJK-NZ2RE-5GH2Z-BABWF'
+    });
 export default {
   data() {
     return {
-      enablezoom:false,
-      enablescroll:false,
-      enablerotate:false,
-      markers: [{
-      iconPath: "../../static/images/biaoshi.png",
-      id: 0,
-      latitude: 31.3119640000,
-      longitude: 120.6020740000,
-      width: 50,
-      height: 50
-      }],
-      
-      
+      enablezoom: false,
+      enablescroll: false,
+      enablerotate: false,
+      markers: [
+        {
+          iconPath: "../../static/images/biaoshi.png",
+          id: 0,
+          latitude: 31.311964,
+          longitude: 120.602074,
+          width: 50,
+          height: 50
+        }
+      ]
+    };
+  },
+  // 查询用户地理位置
+  // onLoad() {
+  //   wx.getLocation({
+  //     type: "gcj02",
+  //     success: res => {
+  //       console.log(res)
+  //       // qqmapsdk.reverseGeocoder({
+  //       //   location: {
+  //       //     latitude: res.latitude,
+  //       //     longitude: res.longitude
+  //       //   },
+  //       //   success: res => {
+  //       //     this.location = res.result.formatted_addresses.recommend;
+  //       //   }
+  //       // });
+  //     }
+  //   });
+  // },
+  methods: {
+    getSelect(){
+      const latitude = this.markers[0].latitude
+      const longitude = this.markers[0].longitude
+      console.log(this.markers[0])
+      wx.openLocation({
+        latitude,
+        longitude,
+        scale: 28,
+        name:"近平江/山塘/苏州站/苏州北站/拙政园/苏州博物馆+地铁口旁【书生】",
+        address:"苏州市 江苏省 中国"
+        // const latitude = res.latitude
+        // const longitude = res.longitude
+        // wx.openLocation({
+        //   latitude,
+        //   longitude,
+        //   scale: 18
+        // })
+      }
+    )
     }
   }
 };
@@ -66,12 +117,12 @@ export default {
 }
 .location .overview h2 {
   font-weight: bold;
-  padding-bottom:20rpx;
+  padding-bottom: 20rpx;
 }
 /* .location .overview .overview-main{
   padding: 0 0 20rpx 0;
 } */
-.location .overview .overview-main p{
+.location .overview .overview-main p {
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
@@ -79,22 +130,22 @@ export default {
   -webkit-line-clamp: 3;
   margin-bottom: 5rpx;
 }
-.location .overview .overview-main .more{
+.location .overview .overview-main .more {
   color: #218380;
 }
-.location .map{
+.location .map {
   padding: 30rpx 0 30rpx 0;
 }
-.location .map-more{
+.location .map-more {
   display: flex;
 }
-.location .map-more img{
+.location .map-more img {
   width: 35rpx;
   height: 40rpx;
   vertical-align: middle;
   padding-right: 15rpx;
 }
-.location .map-more span{
+.location .map-more span {
   vertical-align: middle;
 }
 </style>
